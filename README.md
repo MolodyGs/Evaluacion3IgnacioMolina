24c978d08f81a677a1f5fdfdfb8adc7660e7d241
MolodyGs <ignacio.molina@alumnos.ucn.cl>
2023-12-12 09:35:30 -0300

    commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Evaluacion3IgnacioFront
+# Instalación Frontend
+
+Para la instalación del frontend es necesario tener instalado Node.js.
+
+```
+    npm install
+    npx expo start --web
+```
--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+const api = () => {
+  const [datos, setDatos] = useState(null);
+
+  useEffect(() => {
+
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await fetch('https://172.30.2.127/api/products');
+      const data = await response.json();
+
+      setDatos(data);
+    } catch (error) {
+      console.error('Error al obtener datos de la API:', error);
+    }
+  };
+
+  return (
+    <View>
+      {datos ? (
+        <Text>{JSON.stringify(datos)}</Text>
+      ) : (
+        <Text>Cargando datos...</Text>
+      )}
+    </View>
+  );
+
+};
 export default function List() {
   return (
     <View>
